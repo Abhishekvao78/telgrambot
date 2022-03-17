7f0149f02957b151194230917578a38055f11219
Abhishekvao78 <97386851+Abhishekvao78@users.noreply.github.com>
2022-03-17 10:31:49 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/MRFOXMEDIA"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/MRFOXMEDIA),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/Abhishekvao78/telgrambot')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/MRFOXMEDIA)
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
